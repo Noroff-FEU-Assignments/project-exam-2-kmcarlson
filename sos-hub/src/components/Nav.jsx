e885e260e50153bb5f28fb6c576e58c5210ff28b
Kenneth Carlson <carlson90@hotmail.com>
2024-04-25 14:04:02 +0200

    update nav

--- sos-hub/src/components/Nav.jsx
+++ sos-hub/src/components/Nav.jsx
@@ -1,21 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="hidden md:flex space-x-4">
-      <Link to="/" className="hover:text-pink-200">
-        Home
-      </Link>
-      <Link to="/about" className="hover:text-pink-200">
-        About
-      </Link>
-      <Link to="/contact" className="hover:text-pink-200">
-        Contact
-      </Link>
-      <Link to="/profiles" className="hover:text-pink-200">
-        Profiles
-      </Link>
+    <nav className="flex flex-col md:flex-row space-x-4 md:space-x-8">
+      <div className="md:hidden">
+        <button
+          onClick={toggleMenu}
+          className="text-pink-200 hover:text-pink-500"
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+            ></path>
+          </svg>
+        </button>
+      </div>
+      <div
+        className={`md:flex md:items-center ${
+          isOpen ? "block" : "hidden"
+        } space-y-2 md:space-y-0 md:space-x-8`}
+      >
+        <Link to="/" className="block md:inline-block hover:text-pink-200">
+          Home
+        </Link>
+        <Link to="/about" className="block md:inline-block hover:text-pink-200">
+          About
+        </Link>
+        <Link
+          to="/contact"
+          className="block md:inline-block hover:text-pink-200"
+        >
+          Contact
+        </Link>
+        <Link
+          to="/profiles"
+          className="block md:inline-block hover:text-pink-200"
+        >
+          Profiles
+        </Link>
+      </div>
     </nav>
   );
 };
