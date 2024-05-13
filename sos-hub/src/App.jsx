afefb27260a8ef7b0aab43fd03b8f7b501615f07
Kenneth Carlson <carlson90@hotmail.com>
2024-05-13 11:09:03 +0200

    add useLocation and fix fallow/unfallow

--- sos-hub/src/App.jsx
+++ sos-hub/src/App.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { AuthProvider } from "./components/AuthContext";
 import Account from "./components/Account";
 import Profiles from "./pages/Profiles";
@@ -14,7 +14,7 @@ import Home from "./components/Home";
 import Contact from "./pages/Contact";
 import Posts from "./pages/Posts";
 import Post from "./pages/Post";
-import CreatePost from "./pages/CreatePost"; 
+import CreatePost from "./pages/CreatePost";
 
 function App() {
   return (
@@ -22,23 +22,70 @@ function App() {
       <BrowserRouter>
         <>
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/account" element={<Account />} />
-            <Route path="/profiles" element={<Profiles />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/Contact" element={<Contact />} />
-            <Route path="/posts" element={<Posts />} />
-            <Route path="/posts/:id" element={<Post />} />
-            <Route path="/profile/:name" element={<Profile />} />
-            <Route path="/create-post" element={<CreatePost />} />
-          </Routes>
+          <AppRoutes />
           <Footer />
         </>
       </BrowserRouter>
     </AuthProvider>
+  );
+}
+
+function AppRoutes() {
+  const location = useLocation();
+
+  useEffect(() => {
+
+    const path = location.pathname;
+    let title = "The SOS hub ";
+    switch (path) {
+      case "/":
+        title = "Home - " + title;
+        break;
+      case "/login":
+        title = "Login - " + title;
+        break;
+      case "/register":
+        title = "Register - " + title;
+        break;
+      case "/account":
+        title = "Account - " + title;
+        break;
+      case "/profiles":
+        title = "Profiles - " + title;
+        break;
+      case "/about":
+        title = "About Us - " + title;
+        break;
+      case "/contact":
+        title = "Contact - " + title;
+        break;
+      case "/posts":
+        title = "Posts - " + title;
+        break;
+      case "/create-post":
+        title = "Create Post - " + title;
+        break;
+      default:
+
+        break;
+    }
+    document.title = title;
+  }, [location.pathname]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/account" element={<Account />} />
+      <Route path="/profiles" element={<Profiles />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/posts" element={<Posts />} />
+      <Route path="/posts/:id" element={<Post />} />
+      <Route path="/profile/:name" element={<Profile />} />
+      <Route path="/create-post" element={<CreatePost />} />
+    </Routes>
   );
 }
 
--- sos-hub/src/pages/Profile.jsx
+++ sos-hub/src/pages/Profile.jsx
@@ -4,202 +4,236 @@ import { BASE_URL } from "../constants/ApiUrl";
 import { useAuth } from "../components/AuthContext";
 
 const Profile = () => {
-const { name } = useParams();
-const [profileData, setProfileData] = useState(null);
-const [posts, setPosts] = useState([]);
-const [loading, setLoading] = useState(true);
-const [isFollowing, setIsFollowing] = useState(false);
-const [alreadyFollowingError, setAlreadyFollowingError] = useState(false);
-const { accessToken } = useAuth();
+  const { name } = useParams();
+  const [profileData, setProfileData] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isFollowing, setIsFollowing] = useState(false);
+  const [alreadyFollowingError, setAlreadyFollowingError] = useState(false);
+  const { accessToken, userData } = useAuth();
 
-useEffect(() => {
-const fetchProfile = async () => {
-  try {
-    const response = await fetch(`${BASE_URL}/profiles/${name}?_followers=true&_following=true`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}/profiles/${name}?_followers=true&_following=true`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
 
-    if (response.ok) {
-      const data = await response.json();
-      setProfileData(data);
-      setIsFollowing(data.is_following);
-    } else {
-      console.error("Feil ved henting av profil:", response.statusText);
+        if (response.ok) {
+          const data = await response.json();
+          setProfileData(data);
+          console.log("data", data);
+
+          if (data.followers.find((follower) => follower.name === userData.name)) {
+            setIsFollowing(true);
+          } else {
+            setIsFollowing(false);
+          }
+
+        } else {
+          console.error("Feil ved henting av profil:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Feil ved henting av profil:", error);
+      } finally {
+        setLoading(false);
+
+
+      }
+
+
+    };
+
+    fetchProfile();
+  }, [name, accessToken]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}/profiles/${name}/posts`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setPosts(data);
+        } else {
+          console.error("Feil ved henting av poster:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Feil ved henting av poster:", error);
+      }
+    };
+
+    if (profileData) {
+      fetchPosts();
     }
-  } catch (error) {
-    console.error("Feil ved henting av profil:", error);
-  } finally {
-    setLoading(false);
-  }
+  }, [name, accessToken, profileData]);
 
-};
+  const handleFollow = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/profiles/${name}/follow`, {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
 
-fetchProfile();
-}, [name, accessToken]);
+      if (response.ok) {
+        setIsFollowing(true);
+        console.log("Followed successfully");
 
-useEffect(() => {
-const fetchPosts = async () => {
-  try {
-    const response = await fetch(`${BASE_URL}/profiles/${name}/posts`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      setPosts(data);
-    } else {
-      console.error("Feil ved henting av poster:", response.statusText);
+        const updatedProfileResponse = await fetch(`${BASE_URL}/profiles/${name}?_followers=true&_following=true`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        if (updatedProfileResponse.ok) {
+          const updatedProfileData = await updatedProfileResponse.json();
+          setProfileData(updatedProfileData);
+        } else {
+          console.error("Feil ved henting av oppdatert profil:", updatedProfileResponse.statusText);
+        }
+      } else {
+        const responseData = await response.json();
+        if (responseData.errors && responseData.errors[0].message === "You are already following this profile") {
+          setAlreadyFollowingError(true);
+        } else {
+          console.error("Error following profile:", response.statusText);
+        }
+      }
+    } catch (error) {
+      console.error("Error following profile:", error);
     }
-  } catch (error) {
-    console.error("Feil ved henting av poster:", error);
-  }
-};
+  };
 
-if (profileData) {
-  fetchPosts();
-}
-}, [name, accessToken, profileData]);
+  const handleUnfollow = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/profiles/${name}/unfollow`, {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
 
-const handleFollow = async () => {
-try {
-  const response = await fetch(`${BASE_URL}/profiles/${name}/follow`, {
-    method: "PUT",
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
+      if (response.ok) {
+        setIsFollowing(false);
+        console.log("Unfollowed successfully");
 
-  if (response.ok) {
-    setIsFollowing(true); 
-    console.log("Followed successfully");
-  } else {
-    const responseData = await response.json();
-    if (responseData.errors && responseData.errors[0].message === "You are already following this profile") {
-      setAlreadyFollowingError(true);
-    } else {
-      console.error("Error following profile:", response.statusText);
+        const updatedProfileResponse = await fetch(`${BASE_URL}/profiles/${name}?_followers=true&_following=true`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        if (updatedProfileResponse.ok) {
+          const updatedProfileData = await updatedProfileResponse.json();
+          setProfileData(updatedProfileData);
+        } else {
+          console.error("Feil ved henting av oppdatert profil:", updatedProfileResponse.statusText);
+        }
+      } else {
+        console.error("Error unfollowing profile:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error unfollowing profile:", error);
     }
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
-} catch (error) {
-  console.error("Error following profile:", error);
-}
-};
 
-const handleUnfollow = async () => {
-try {
-  const response = await fetch(`${BASE_URL}/profiles/${name}/unfollow`, {
-    method: "PUT",
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
+  return (
+    <div>
+      {alreadyFollowingError && (
+        <p>You are already following this profile</p>
+      )}
+      {profileData && (
+        <div className="container mx-auto py-8">
+          <h3 className="text-2xl font-bold mb-4">Min profil</h3>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="mb-4">
+              <h2 className="text-lg font-semibold">Profil:</h2>
+              <p className="text-gray-800">{profileData.name}</p>
+            </div>
+            <div className="mb-4">
+              <h2 className="text-lg font-semibold">Email:</h2>
+              <p className="text-gray-800">{profileData.email}</p>
+            </div>
+            <div className="mb-4">
+              <h2 className="text-lg font-semibold">Banner:</h2>
+              {profileData.banner ? (
+                <img
+                  src={profileData.banner}
+                  alt="Banner"
+                  className="w-full h-auto object-cover max-h-40"
+                />
+              ) : (
+                <p className="text-gray-800">No banner available</p>
+              )}
+            </div>
+            <div className="mb-4">
+              <h2 className="text-lg font-semibold">Avatar:</h2>
+              {profileData.avatar ? (
+                <img
+                  src={profileData.avatar}
+                  alt="Avatar"
+                  className="w-full h-auto object-cover max-h-40"
+                />
+              ) : (
+                <p className="text-gray-800">No avatar available</p>
+              )}
+            </div>
+            <div className="mb-4">
+              <h2 className="text-lg font-semibold">Number of posts:</h2>
+              <p className="text-gray-800">{profileData._count.posts}</p>
+            </div>
+            <div className="mb-4">
+              <div className="mb-4">
+                <h2 className="text-lg font-semibold">Followers:</h2>
+                <ul>
+                  {profileData.followers.map((follower, index) => (
+                    <li key={index}>
+                      <Link to={`/profile/${follower.name}`}>{follower.name}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mb-4">
+                <h2 className="text-lg font-semibold">Following:</h2>
+                <ul>
+                  {profileData.following.map((following, index) => (
+                    <li key={index}>
+                      <Link to={`/profile/${following.name}`}>{following.name}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
 
-  if (response.ok) {
-    setIsFollowing(false); 
-    console.log("Unfollowed successfully");
-  } else {
-    console.error("Error unfollowing profile:", response.statusText);
-  }
-} catch (error) {
-  console.error("Error unfollowing profile:", error);
-}
-};
 
-if (loading) {
-return <div>Loading...</div>;
-}
-
-return (
-<div>
-  {alreadyFollowingError && (
-    <p>You are already following this profile</p>
-  )}
-  {profileData && (
-    <div className="container mx-auto py-8">
-      <h3 className="text-2xl font-bold mb-4">Min profil</h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div className="mb-4">
-          <h2 className="text-lg font-semibold">Profil:</h2>
-          <p className="text-gray-800">{profileData.name}</p>
-        </div>
-        <div className="mb-4">
-          <h2 className="text-lg font-semibold">Email:</h2>
-          <p className="text-gray-800">{profileData.email}</p>
-        </div>
-        <div className="mb-4">
-          <h2 className="text-lg font-semibold">Banner:</h2>
-          {profileData.banner ? (
-            <img
-              src={profileData.banner}
-              alt="Banner"
-              className="w-full h-auto object-cover max-h-40"
-            />
-          ) : (
-            <p className="text-gray-800">No banner available</p>
-          )}
-        </div>
-        <div className="mb-4">
-          <h2 className="text-lg font-semibold">Avatar:</h2>
-          {profileData.avatar ? (
-            <img
-              src={profileData.avatar}
-              alt="Avatar"
-              className="w-full h-auto object-cover max-h-40"
-            />
-          ) : (
-            <p className="text-gray-800">No avatar available</p>
-          )}
-        </div>
-        <div className="mb-4">
-          <h2 className="text-lg font-semibold">Number of posts:</h2>
-          <p className="text-gray-800">{profileData._count.posts}</p>
-        </div>
-        <div className="mb-4">
-        <div className="mb-4">
-      <h2 className="text-lg font-semibold">Followers:</h2>
+            </div>
+          </div>
+          <h3 className="text-2xl font-bold mb-4">Posts by {profileData.name}</h3>
           <ul>
-            {profileData.followers.map((follower, index) => (
-              <li key={index}>
-                <Link to={`/profile/${follower.name}`}>{follower.name}</Link>
+            {posts.map((post) => (
+              <li key={post.id}>
+                <Link to={`/posts/${post.id}`}>{post.title}</Link>
               </li>
             ))}
           </ul>
+          {isFollowing ? (
+            <button onClick={handleUnfollow}>Unfollow</button>
+          ) : (
+            <button onClick={handleFollow}>Follow</button>
+          )}
         </div>
-        <div className="mb-4">
-          <h2 className="text-lg font-semibold">Following:</h2>
-          <ul>
-            {profileData.following.map((following, index) => (
-              <li key={index}>
-                <Link to={`/profile/${following.name}`}>{following.name}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-
-        </div>
-      </div>
-      <h3 className="text-2xl font-bold mb-4">Posts by {profileData.name}</h3>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Link to={`/posts/${post.id}`}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
-      {isFollowing ? (
-        <button onClick={handleUnfollow}>Unfollow</button>
-      ) : (
-        <button onClick={handleFollow}>Follow</button>
       )}
     </div>
-  )}
-</div>
-);
+  );
 };
 
 export default Profile;
